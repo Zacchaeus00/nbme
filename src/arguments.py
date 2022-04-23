f7957b0e670f66938f3b2f681a6a18be66bbadae
Zacchaeus14 <yw3642@nyu.edu>
2022-04-24 07:41:29 +0800

    update

---
+++ src/arguments.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='')
+    arg = parser.add_argument
+    arg('--pretrained_checkpoint', type=str, default='microsoft/deberta-base')
+    arg('--data_path', type=str, default='../data/train_processed.pkl')
+    arg('--epochs', type=int, default=5)
+    arg('--batch_size', type=int, default=16)
+    arg('--accumulation_steps', type=int, default=1)
+    arg('--lr', type=float, default=2e-5)
+    arg('--weight_decay', type=float, default=0.01)
+    arg('--seed', type=int, default=42)
+    return parser.parse_args()
--- src/train.py
+++ src/train.py
@@ -1,14 +1,41 @@
 import pandas as pd
 from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import TrainingArguments, Trainer
 
 from data_utils import NBMEDataset
+from arguments import parse_args
+from utils import seed_everything
 
+cfg = parse_args()
 fold = 0
-df = pd.read_pickle('../data/train_processed.pkl')
-train_df = df[df['fold']!=fold].reset_index(drop=True)
-val_df = df[df['fold']==fold].reset_index(drop=True)
-tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-base")
+df = pd.read_pickle(cfg.data_path)
+train_df = df[df['fold'] != fold].reset_index(drop=True)
+val_df = df[df['fold'] == fold].reset_index(drop=True)
+tokenizer = AutoTokenizer.from_pretrained(cfg.pretrained_checkpoint)
 train_dataset = NBMEDataset(tokenizer, train_df)
 val_dataset = NBMEDataset(tokenizer, val_df)
-print(len(train_dataset), len(val_dataset))
-print(train_dataset[0])
+seed_everything(cfg.seed)
+args = TrainingArguments(
+        output_dir=f"./ckpt",
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        learning_rate=cfg.lr,
+        per_device_train_batch_size=cfg.batch_size,
+        per_device_eval_batch_size=cfg.batch_size,
+        num_train_epochs=cfg.epochs,
+        weight_decay=cfg.weight_decay,
+        load_best_model_at_end=True,
+        fp16=True,
+        report_to='none',
+        dataloader_num_workers=4
+)
+model = AutoModelForTokenClassification.from_pretrained(cfg.pretrained_checkpoint, num_labels=2)
+trainer = Trainer(
+        model,
+        args,
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+        tokenizer=tokenizer,
+        # compute_metrics=compute_metrics
+)
+trainer.train()
