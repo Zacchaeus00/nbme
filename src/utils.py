71ee0111c72ef186d3a5cda984781c2ba81ac37e
Zacchaeus14 <yw3642@nyu.edu>
2022-04-25 06:11:10 +0800

    fix roberta tok

--- src/utils.py
+++ src/utils.py
@@ -32,9 +32,11 @@ def get_tokenizer(checkpoint):
     if 'deberta-v3' in checkpoint:
         from transformers import DebertaV2TokenizerFast
         return DebertaV2TokenizerFast.from_pretrained(checkpoint)
+    if 'roberta-large' in checkpoint:
+        return AutoTokenizer.from_pretrained(checkpoint, trim_offsets=False)
     return AutoTokenizer.from_pretrained(checkpoint)
 
 if __name__ == "__main__":
     # tok = get_tokenizer('microsoft/deberta-v3-large')
     tok = get_tokenizer('roberta-large')
-    tok(['i am a student'], return_offsets_mapping=True)
+    print(tok(['i am a student'], return_offsets_mapping=True))
