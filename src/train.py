357e7181eafba47ff8c601fd8f346c58235ff052
Zacchaeus14 <yw3642@nyu.edu>
2022-04-24 11:03:35 +0800

    fix: indent

--- src/train.py
+++ src/train.py
@@ -15,26 +15,26 @@ val_df = df[df['fold'] == fold].reset_index(drop=True)
 tokenizer = AutoTokenizer.from_pretrained(cfg.pretrained_checkpoint)
 seed_everything(cfg.seed)
 args = TrainingArguments(
-        output_dir=f"./ckpt",
-        evaluation_strategy="epoch",
-        save_strategy="epoch",
-        learning_rate=cfg.lr,
-        per_device_train_batch_size=cfg.batch_size,
-        per_device_eval_batch_size=cfg.batch_size,
-        num_train_epochs=cfg.epochs,
-        weight_decay=cfg.weight_decay,
-        load_best_model_at_end=True,
-        fp16=True,
-        report_to='none',
-        dataloader_num_workers=4,
-        group_by_length=True
+    output_dir=f"./ckpt",
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    learning_rate=cfg.lr,
+    per_device_train_batch_size=cfg.batch_size,
+    per_device_eval_batch_size=cfg.batch_size,
+    num_train_epochs=cfg.epochs,
+    weight_decay=cfg.weight_decay,
+    load_best_model_at_end=True,
+    fp16=True,
+    report_to='none',
+    dataloader_num_workers=4,
+    group_by_length=True
 )
 trainer = Trainer(
-        AutoModelForTokenClassification.from_pretrained(cfg.pretrained_checkpoint, num_labels=2),
-        args,
-        train_dataset=NBMEDataset(tokenizer, train_df),
-        eval_dataset=NBMEDataset(tokenizer, val_df),
-        tokenizer=tokenizer,
-        data_collator=DataCollatorForTokenClassification(tokenizer),
+    AutoModelForTokenClassification.from_pretrained(cfg.pretrained_checkpoint, num_labels=2),
+    args,
+    train_dataset=NBMEDataset(tokenizer, train_df),
+    eval_dataset=NBMEDataset(tokenizer, val_df),
+    tokenizer=tokenizer,
+    data_collator=DataCollatorForTokenClassification(tokenizer),
 )
 trainer.train()
