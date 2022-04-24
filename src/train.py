a95fe4852ef64d0c871d2e6ed26bf39a09733a79
Zacchaeus14 <yw3642@nyu.edu>
2022-04-24 11:34:49 +0800

    update wandb settings

--- src/train.py
+++ src/train.py
@@ -1,13 +1,17 @@
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+os.environ["WANDB_PROJECT"] = "nbme"
+os.environ["WANDB_ENTITY"] = "zacchaeus"
+
 import pandas as pd
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 from transformers import TrainingArguments, Trainer
 from transformers import DataCollatorForTokenClassification
-import os
-os.environ['TOKENIZERS_PARALLELISM='] = False
+import wandb
 
 from data_utils import NBMEDataset
 from arguments import parse_args
-from utils import seed_everything
+from utils import seed_everything, get_time
 
 cfg = parse_args()
 fold = 0
@@ -16,8 +20,9 @@ train_df = df[df['fold'] != fold].reset_index(drop=True)
 val_df = df[df['fold'] == fold].reset_index(drop=True)
 tokenizer = AutoTokenizer.from_pretrained(cfg.pretrained_checkpoint)
 seed_everything(cfg.seed)
+name = get_time()
 args = TrainingArguments(
-    output_dir=f"./ckpt",
+    output_dir=f"../ckpt/{name}",
     evaluation_strategy="epoch",
     save_strategy="epoch",
     learning_rate=cfg.lr,
@@ -26,10 +31,12 @@ args = TrainingArguments(
     num_train_epochs=cfg.epochs,
     weight_decay=cfg.weight_decay,
     load_best_model_at_end=True,
-    fp16=True,
-    report_to='none',
-    dataloader_num_workers=4,
-    group_by_length=True
+    warmup_ratio=0.2,
+    # fp16=True,
+    report_to='wandb',
+    # dataloader_num_workers=4,
+    group_by_length=True,
+    run_name=name
 )
 trainer = Trainer(
     AutoModelForTokenClassification.from_pretrained(cfg.pretrained_checkpoint, num_labels=2),
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import random
 
 import numpy as np
 import torch
+import datetime
 
 
 def seed_everything(seed=42):
@@ -13,3 +14,9 @@ def seed_everything(seed=42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
+
+
+def get_time():
+    timenow = str(datetime.datetime.now()).split('.')[0]
+    timenow = '-'.join(timenow.split())
+    return timenow
