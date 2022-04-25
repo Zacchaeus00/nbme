285de1cac844d9cbe5e3f7a088833031c7371ee7
Zacchaeus14 <yw3642@nyu.edu>
2022-04-25 08:13:50 +0800

    fix: bart init weight

--- src/model_utils.py
+++ src/model_utils.py
@@ -15,11 +15,17 @@ class NBMEModel(nn.Module):
 
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            if hasattr(self.config, 'initializer_range'):
+                module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            else:
+                module.weight.data.normal_(mean=0.0, std=0.02)
             if module.bias is not None:
                 module.bias.data.zero_()
         elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            if hasattr(self.config, 'initializer_range'):
+                module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            else:
+                module.weight.data.normal_(mean=0.0, std=0.02)
             if module.padding_idx is not None:
                 module.weight.data[module.padding_idx].zero_()
         elif isinstance(module, nn.LayerNorm):
