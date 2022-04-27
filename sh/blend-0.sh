fcae7e3f9d81bde38bc936022cabfeb89100f89e
Zacchaeus14 <yw3642@nyu.edu>
2022-04-27 22:03:57 +0800

    fix: blend sh

--- sh/blend-0.sh
+++ sh/blend-0.sh
@@ -9,7 +9,7 @@
 #SBATCH --mail-user=yw3642@nyu.edu  
 #SBATCH --output=log/%x-%A.out
 #SBATCH --error=log/%x-%A.err
-#SBATCH --gres=gpu:0
+#SBATCH --constraint=cpu # use this if you want to only use cpu
 #SBATCH -p aquila                   
 
 module purge                        
@@ -26,10 +26,11 @@ python -u blend.py --result_dirs \
 /gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-20:11:47-edb5 \
 /gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-20:11:47-22c0 \
 /gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-20:11:47-952e \
-/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-13:16:38-8c3f \
+/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-20:11:47-c8b3 \
 /gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-17:57:25-9d71 \
 /gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-09:27:10-4c7e \
-/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-27-02:53:06-6b2f
+/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-27-02:53:06-6b2f \
+--n_trials 1000
 #muppet-roberta-large
 #deberta-large
 #deberta-v3-large
