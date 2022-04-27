f0622cf0d4437dce4c2a373b2cd111abbea0cb78
Zacchaeus14 <yw3642@nyu.edu>
2022-04-27 21:54:40 +0800

    fix: blend sh

--- sh/blend-0.sh
+++ sh/blend-0.sh
@@ -9,9 +9,8 @@
 #SBATCH --mail-user=yw3642@nyu.edu  
 #SBATCH --output=log/%x-%A.out
 #SBATCH --error=log/%x-%A.err
-#SBATCH --gres=gpu:1                
+#SBATCH --gres=gpu:0
 #SBATCH -p aquila                   
-#SBATCH --nodelist=agpu7            
 
 module purge                        
 module load anaconda3 cuda/11.1.1              
@@ -24,11 +23,18 @@ echo "START"
 source deactivate
 source /gpfsnyu/packages/anaconda3/5.2.0/bin/activate kaggle          
 python -u blend.py --result_dirs \
-/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-20:11:47-edb5 \#muppet-roberta-large
-/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-20:11:47-22c0 \#deberta-large
-/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-20:11:47-952e \#deberta-v3-large
-/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-13:16:38-8c3f \#bart-large
-/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-17:57:25-9d71 \#bart-large-mnli
-/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-09:27:10-4c7e \#roberta-large
-/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-27-02:53:06-6b2f \#roberta-large (seed1)
+/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-20:11:47-edb5 \
+/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-20:11:47-22c0 \
+/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-20:11:47-952e \
+/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-13:16:38-8c3f \
+/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-17:57:25-9d71 \
+/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-26-09:27:10-4c7e \
+/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-27-02:53:06-6b2f
+#muppet-roberta-large
+#deberta-large
+#deberta-v3-large
+#bart-large
+#bart-large-mnli
+#roberta-large
+#roberta-large (seed1)
 echo "FINISH"                       
