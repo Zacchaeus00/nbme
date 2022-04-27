42dfc8bb2744e4d08288d7b01a5f5b8d0639bfe5
Zacchaeus14 <yw3642@nyu.edu>
2022-04-27 21:11:04 +0800

    add deberta base finetune exp

---
+++ sh/finetune-deberta-base-ep10.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=finetune-deberta-base-ep10
+#SBATCH --nodes=1                   
+#SBATCH --ntasks=1                  
+#SBATCH --cpus-per-task=8           
+#SBATCH --mem=16GB                  
+#SBATCH --time=167:00:00
+#SBATCH --mail-type=END             
+#SBATCH --mail-user=yw3642@nyu.edu  
+#SBATCH --output=log/%x-%A.out
+#SBATCH --error=log/%x-%A.err
+#SBATCH --gres=gpu:1                
+#SBATCH -p aquila                   
+#SBATCH --nodelist=agpu7            
+
+module purge                        
+module load anaconda3 cuda/11.1.1              
+
+nvidia-smi
+nvcc --version
+cd /gpfsnyu/scratch/yw3642/nbme/src     
+
+echo "START"               
+source deactivate
+source /gpfsnyu/packages/anaconda3/5.2.0/bin/activate kaggle          
+python -u train.py --pretrained_checkpoint /gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-27-19:37:49-3690/checkpoint-26350 \
+--epochs 10 --batch_size 16 --accumulation_steps 1 --lr 3e-5 --weight_decay 0.0 --seed 50000
+echo "FINISH"                       
