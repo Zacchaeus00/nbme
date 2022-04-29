71e6a9d3ba77ab403c5e79f0376459c0601b2e18
Zacchaeus14 <yw3642@nyu.edu>
2022-04-29 18:13:54 +0800

    add pl infer rob base exp

--- sh/pl-infer-2.sh
+++ sh/pl-infer-2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=pl-infer-2
+#SBATCH --job-name=pl-infer-roberta-base
 #SBATCH --nodes=1                   
 #SBATCH --ntasks=1                  
 #SBATCH --cpus-per-task=8           
-#SBATCH --mem=128GB
+#SBATCH --mem=32GB
 #SBATCH --time=167:00:00
 #SBATCH --mail-type=END             
 #SBATCH --mail-user=yw3642@nyu.edu  
@@ -24,11 +24,6 @@ echo "START"
 source deactivate
 source /gpfsnyu/packages/anaconda3/5.2.0/bin/activate kaggle          
 python -u pl_infer.py \
---blend_log /gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-28-23:22:20-efef/blend-0eeb.json \
---pretrained_checkpoints \
-/gpfsnyu/scratch/yw3642/hf-models/roberta-large \
-/gpfsnyu/scratch/yw3642/hf-models/facebook_muppet-roberta-large \
---model_dirs \
-/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-28-20:19:05-967e \
-/gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-28-23:22:20-efef
+--pretrained_checkpoint /gpfsnyu/scratch/yw3642/hf-models/roberta-base \
+--model_dir /gpfsnyu/scratch/yw3642/nbme/ckpt/2022-04-29-08:40:24-f2bb
 echo "FINISH"                       
