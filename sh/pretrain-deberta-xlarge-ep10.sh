a420a76d2bedcdcf518d7382c2f976b91d1420a4
Zacchaeus14 <yw3642@nyu.edu>
2022-05-01 02:24:02 +0800

    fix: deberta xl oom

--- sh/pretrain-deberta-xlarge-ep10.sh
+++ sh/pretrain-deberta-xlarge-ep10.sh
@@ -24,5 +24,5 @@ echo "START"
 source deactivate
 source /gpfsnyu/packages/anaconda3/5.2.0/bin/activate kaggle          
 python -u pretrain.py --pretrained_checkpoint /gpfsnyu/scratch/yw3642/hf-models/microsoft_deberta-xlarge --epochs 10 \
---batch_size 2 --accumulation_steps 1 --lr 1e-5 --weight_decay 0.0 --mlm_prob 0.2 --seed 3296
+--batch_size 3 --accumulation_steps 1 --lr 1e-5 --weight_decay 0.0 --mlm_prob 0.2 --seed 3296
 echo "FINISH"                       
