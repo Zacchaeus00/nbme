08b7f72f7f3e53b490b0eacbec0190e6f9c61c0f
Zacchaeus14 <yw3642@nyu.edu>
2022-05-02 18:04:46 +0800

    fix: debxl fold2 oom on a10

--- sh/pl-train-3a81-deberta-xlarge-fold2.sh
+++ sh/pl-train-3a81-deberta-xlarge-fold2.sh
@@ -11,7 +11,7 @@
 #SBATCH --error=log/%x-%A.err
 #SBATCH --gres=gpu:1                
 #SBATCH -p aquila                   
-#SBATCH --nodelist=agpu8
+#SBATCH --nodelist=agpu7
 
 module purge                        
 module load anaconda3 cuda/11.1.1              
