95d300fbcfd8cfffe9625c75b82fb2d799f469c5
Zacchaeus14 <yw3642@nyu.edu>
2022-05-03 10:05:25 +0800

    add debxl eval exp

---
+++ sh/eval-3a81-deberta-xlarge.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=eval-3a1-deberta-xlarge
+#SBATCH --nodes=1                   
+#SBATCH --ntasks=1                  
+#SBATCH --cpus-per-task=8           
+#SBATCH --mem=16GB                  
+#SBATCH --time=167:00:00
+#SBATCH --mail-type=END             
+#SBATCH --mail-user=yw3642@nyu.edu  
+#SBATCH --output=log/%x-%A.out
+#SBATCH --error=log/%x-%A.err
+#SBATCH --gres=gpu:1                
+#SBATCH -p aquila                   
+#SBATCH --nodelist=agpu7            
+
+module purge                        
+module load anaconda3 cuda/11.1.1              
+
+nvidia-smi
+nvcc --version
+cd /gpfsnyu/scratch/yw3642/nbme/src     
+
+echo "START"               
+source deactivate
+source /gpfsnyu/packages/anaconda3/5.2.0/bin/activate kaggle          
+python -u eval.py --pretrained_checkpoint /gpfsnyu/scratch/yw3642/hf-models/microsoft_deberta-xlarge \
+--model_dir /gpfsnyu/scratch/yw3642/nbme/ckpt/2022-05-01-23:06:10-147b
+echo "FINISH"                       
